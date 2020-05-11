470ca3519c01d663a627c3a6318d28999ff48280
Melstolit <melster19@hotmail.com>
2020-05-11 14:21:39 -0400

    Modified hello world page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,26 +6,11 @@
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3>Quantum Libraries</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li><a href="https://qiskit.org/" target="_blank" rel="noopener">Qiskit</a></li>
+      <li><a href="https://www.microsoft.com/en-us/quantum/development-kit" target="_blank" rel="noopener">MS QDK</a></li>
+      <li><a href="https://cirq.readthedocs.io/en/stable/" target="_blank" rel="noopener">Cirq</a></li>
     </ul>
   </div>
 </template>
@@ -53,6 +38,6 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: #2099d3;
 }
 </style>
